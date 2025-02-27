0cfae63e390e367f72832ca106271bcfcd906101
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-02-27 08:43:22 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @Codexownerr
+# Join Update Channel For Amazing Bot https://t.me/codexbotmaker
+# Ask Doubt on telegram @codexdisscus
 
 import re
 import logging
